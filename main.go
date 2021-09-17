fb0755ff51f87cc7174d7ec0343eddf4d5d19f62
yurifrl <yurifl03@gmail.com>
2021-09-17 00:53:53 -0300

    wip

--- main.go
+++ main.go
@@ -17,15 +17,16 @@ import (
 )
 
 func main() {
+	readAndAppend("text.log")
 	// The "HandleFunc" method accepts a path and a function as arguments
 	// (Yes, we can pass functions as arguments, and even treat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-	http.HandleFunc("/", handler)
+	// // However, the handler function has to have the appropriate signature (as described by the "handler" function below)
+	// http.HandleFunc("/", handler)
 
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+	// // After defining our server, we finally "listen and serve" on port 8080
+	// // The second argument is the handler, which we will come to later on, but for now it is left as nil,
+	// // and the handler defined above (in "HandleFunc") is used
+	// http.ListenAndServe(":8080", nil)
 }
 
 // "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
@@ -45,28 +46,29 @@ func readAndAppend(fileName string) string {
 	fileDescriptor, err := os.Stat(fileName)
 	lineBreak := []byte("\n")
 	lineBreakSize := int64(len(lineBreak))
-	curr := fileDescriptor.Size() - lineBreakSize
+	current := fileDescriptor.Size() - lineBreakSize
 	entry := 0
-	if curr > 0 {
+	if current > 0 {
 		lineBuf := make([]byte, lineBreakSize)
 
 		for {
-			n, _ := file.ReadAt(lineBuf, curr)
+			n, _ := file.ReadAt(lineBuf, current)
 
 			if bytes.Equal(lineBuf[:n], lineBreak) {
 				break
 			}
 
-			curr -= lineBreakSize
+			current -= lineBreakSize
 		}
 	}
 
-	numberBuf := make([]byte, curr+lineBreakSize-fileDescriptor.Size())
-	_, err = file.ReadAt(numberBuf, curr+lineBreakSize)
+	numberBuf := make([]byte, current+lineBreakSize-fileDescriptor.Size())
+	_, err = file.ReadAt(numberBuf, current+lineBreakSize)
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println(numberBuf)
 	entry, err = strconv.Atoi(string(numberBuf))
 	if err != nil {
 		panic(err)
